0bf728274a64d75108ce7198620ca728487d989a
albertogonda <alberto.gonda@xskyinc.com>
2018-03-06 22:43:47 -0800

    Added recipesSelected event

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+  private _recipesSelected: EventEmitter<{}> = new EventEmitter();
 
+  @Output()
+  public get recipesSelected(): EventEmitter<{}> {
+    return this._recipesSelected;
+  }
+
+  public onRecipesSelected(): void {
+    this._recipesSelected.emit();
+  }
 }
