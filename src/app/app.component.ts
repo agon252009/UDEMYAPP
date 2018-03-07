319a3ada36c917fd39544678ce91c9e9c5af72f8
albertogonda <alberto.gonda@xskyinc.com>
2018-03-06 23:03:09 -0800

    added selectShopping list logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+
+export class AppComponent {
+
+  private _isRecipesSelected = true;
+  private _isShoppingListSelected = false;
+
+  public get isRecipesSelected(): boolean {
+    return this._isRecipesSelected;
+  }
+
+  public get isShoppingListSelected(): boolean {
+    return this._isShoppingListSelected;
+  }
+
+  public selectRecipes(): void {
+    this._isRecipesSelected = true;
+    this._isShoppingListSelected = false;
+  }
+
+  public selectShoppingList(): void {
+    this._isShoppingListSelected = true;
+    this._isRecipesSelected = false;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,14 +5,24 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
-  private _recipesSelected: EventEmitter<{}> = new EventEmitter();
+  private _recipesSelected = new EventEmitter();
+  private _shoppingListSelected = new EventEmitter();
 
   @Output()
   public get recipesSelected(): EventEmitter<{}> {
     return this._recipesSelected;
   }
 
+  @Output()
+  public get shoppingListSelected(): EventEmitter<{}> {
+    return this._shoppingListSelected;
+  }
+
   public onRecipesSelected(): void {
     this._recipesSelected.emit();
+  }
+
+  public onShoppingListSelected(): void {
+    this._shoppingListSelected.emit();
   }
 }
