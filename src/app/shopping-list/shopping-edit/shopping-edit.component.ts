107359b00b14326410b43d1f8f81fbf80b84350f
Alberto <agon2520092gmail.com>
2018-03-09 11:09:25 -0800

    Implemented two way datagbinding using ngModel

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
-import {Ingredient} from '../../shared/ingredient.model';
+import { Component, EventEmitter, Output, ElementRef} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,20 +9,31 @@ import {Ingredient} from '../../shared/ingredient.model';
 
 export class ShoppingEditComponent {
   private _addIngredientEvent: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+  private _name: string;
+  private _amount: number;
 
-  // @ViewChild('ingredientNameInput')
-  // private _ingredientNameInput: ElementRef;
+  public get name(): string {
+    return this._name;
+  }
 
-  // @ViewChild('ingredientAmountInput')
-  // private _ingredientAmountInput: ElementRef;
+  public set name(value: string) {
+    this._name = value;
+  }
 
+  public get amount(): number {
+    return this._amount;
+  }
+
+  public set amount(value: number) {
+    this._amount = value;
+  }
 
   @Output()
   public get addIngredientEvent(): EventEmitter<Ingredient> {
     return this._addIngredientEvent;
   }
 
-  public onAddIngredientButtonClick(name: string, amount: number): void {
-     this._addIngredientEvent.emit(new Ingredient(name, amount));
+  public onAddIngredientButtonClick(): void {
+    this._addIngredientEvent.emit(new Ingredient(this._name, this._amount));
   }
 }
