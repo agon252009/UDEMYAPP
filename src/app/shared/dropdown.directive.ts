a8b5b5e22c188e04322d9fcc7809d1bd940a2d69
albertogonda <alberto.gonda@xskyinc.com>
2018-03-20 23:14:18 -0700

    Added menu functionality

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  private _open = false;
+  @HostListener('click')
 
-  constructor() { }
+  onclick() {
+    this._open = !this._open;
+    this.toggleShow();
+  }
 
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {}
+
+  private toggleShow() {
+    if (this._open) {
+      this._renderer.addClass(this._elementRef.nativeElement.nextElementSibling, 'show');
+    } else {
+      this._renderer.removeClass(this._elementRef.nativeElement.nextElementSibling, 'show');
+    }
+  }
 }
